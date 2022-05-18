38a01937ff8618c33d772af42edcca1ff0ebd9a1
fern <792847327@qq.com>
2022-05-18 09:29:51 +0900

    Create torch_utils.py

---
+++ pytorch/utils/torch_utils.py
@@ -0,0 +1,48 @@
+import os
+import logging
+import time
+
+import torch
+import torch.backends.cudnn as cudnn
+
+
+logger = logging.getLogger(__name__)
+
+# yolov5 참조         
+def init_torch_seeds(seed=0):
+    torch.manual_seed(seed)
+
+    # Speed-reproducibility tradeoff https://pytorch.org/docs/stable/notes/randomness.html
+    if seed == 0:  # slower, more reproducible
+        cudnn.deterministic = True
+        cudnn.benchmark = False
+    else:  # faster, less reproducible
+        cudnn.deterministic = False
+        cudnn.benchmark = True
+
+        
+def select_device(device='', batch_size=None):
+    # device = 'cpu' or '0' or '0,1,2,3'
+    cpu_request = device.lower() == 'cpu'
+    if device and not cpu_request:  # if device requested other than 'cpu'
+        os.environ['CUDA_VISIBLE_DEVICES'] = device  # set environment variable
+        assert torch.cuda.is_available(), 'CUDA unavailable, invalid device %s requested' % device  # check availablity
+
+    cuda = False if cpu_request else torch.cuda.is_available()
+    if cuda:
+        c = 1024 ** 2  # bytes to MB
+        ng = torch.cuda.device_count()
+        if ng > 1 and batch_size:  # check that batch_size is compatible with device_count
+            assert batch_size % ng == 0, 'batch-size %g not multiple of GPU count %g' % (batch_size, ng)
+        x = [torch.cuda.get_device_properties(i) for i in range(ng)]
+        s = 'Using CUDA '
+        for i in range(0, ng):
+            if i == 1:
+                s = ' ' * len(s)
+            logger.info("%sdevice%g _CudaDeviceProperties(name='%s', total_memory=%dMB)" %
+                        (s, i, x[i].name, x[i].total_memory / c))
+    else:
+        logger.info('Using CPU')
+
+    logger.info('')  # skip a line
+    return torch.device('cuda' if cuda else 'cpu')
